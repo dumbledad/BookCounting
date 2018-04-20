e2647f20737d87889941711e0f23de30eb717043
Tim Regan <timregan@microsoft.com>
2018-04-20 13:04:39 +0100

    Book counting with nested directories
    And divide by zero fix

--- BookCounting/BookCounting.py
+++ BookCounting/BookCounting.py
@@ -3,73 +3,117 @@
 import os, io
 from functools import reduce
 
-path = "C:\\Users\\timregan\\Dropbox\\Projects\\Visualization\\Projectwork\\Miles2\\Texts\\Squished\\"
+path = "C:\\Users\\timregan\\Dropbox\\Projects\\Visualization\\Projectwork\\Miles2\\Texts\\"
 
 # Concepts supplied by Miles, lower-cased and cut to one word only
-communityWords = ["community", "opportunity", "responsibility", "society", "rights"]
-religionWords = ["religion", "christian", "belief", "conviction"]
+communityWords = ["community", "communities", "society", "civil society", "civil", "people", "cohesion", "individual", "alone", "stakeholder", "stake", "decency", "mutual", "local", "regional", "neighbourhood", "relation", "relationship", "relationships", "active", "activism", "collective", "behaviour", "group", "social", "culture", "allegiance", "trust", "charity", "autonomy", "compassion", "owe", "partner", "diversity", "liberty", "communication", "emancipate", "social"] 
+religionWords = ["christian", "protestant", "catholic", "justice", "secular", "atheist", "belief", "tolerance", "experiences", "moral", "ethical", "compassion", "faith", "christian", "congregation", "church", "religious", "religion", "gospels", "christ", "god"]
+socialWords = ["social", "socialism", "mutual", "public", "personal", "self-interest", "group", "community", "interdependence", "cooperative", "reciprocal", "needs"]
+familyWords = ["family", "local", "children", "lives", "shelter", "parent", "parents", "home", "homes", "wife", "husband", "sex", "families"]
+economicsWords = ["work", "economic", "market", "economics", "invest", "competition", "stability", "growth", "nationalisation", "ownership", "compete", "neoliberal", "profit", "risk", "productive", "efficiency", "inequality", "enterprise", "regulation", "dynamic", "tech", "prudence", "prosperity", "tax", "consumers"]
+educationWords = ["school", "schools", "maths", "nurseries", "university", "local authorities", "young", "education", "young people", "skills", "literacy", "educational"] 
+healthWords = ["poverty", "nhs", "n.h.s.", "health", "waiting list", "hospital", "care", "clinical", "patient"] 
+crimeWords = ["behaviour", "culture", "security", "discipline", "fear", "poverty", "urban", "offenders", "violent crime", "crime", "police", "prosecute", "drugs", "victims"]
+environmentWords = ["environment", "pollution", "sustainable", "sustainability", "climate change", "countryside", "forests", "carbon", "clean", "rural"]
+welfareWords = ["welfare", "compassion", "culture", "depend", "dependence", "benefits", "insecure", "survival", "social security", "unemployment"] 
+europeWords = ["europe", "eu", "union", "brussels"]
+foreignPolicyWords = ["global", "nation", "security", "international", "peace", "war"]
+constitutionWords = ["authority", "rule", "constitution", "devolution", "House of Lords", "government"]
+thirdWayWords = ["third way", "values", "duties", "rights", "stake", "stakeholder", "stakeholders", "responsibility", "responsibilities", "change", "reform", "middle", "third way", "justice", "idea", "ideology", "opportunity", "equal", "obligation", "obligations", "freedom", "stability", "ethic", "commit", "history", "identity", "culture", "trust", "progressive", "enterprise", "philosophy", "modern", "choice", "fair", "reciprocal", "entrepreneur", "devolution", "open", "accountability", "consensus", "redistribution"]
 
-# Generic stop words. We could do more to tailor these to our corpus
-stopWords = ["a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't", "cannot", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"]
+# Generic and project specific stop words
+stopWords = ["a", "about", "after", "all", "also", "an", "and", "any", "around ", "as", "at", "back", "be", "be", "because", "blair", "but", "by", "can", "come", "conservative", "could", "course", "day", "do", "do", "even", "ever", "first", "first", "for", "from", "get", "get", "give", "go", "good", "got", "great", "have", "he", "her", "him", "his", "how", "however", "i", "if", "important", "in", "into", "it", "its", "just", "just", "know", "labour", "like", "look", "major", "make", "make", "may", "me", "means", "measures", "might", "most", "my", "new", "no", "not", "now", "of", "on", "one", "only", "or", "other", "our", "out", "over", "people", "plan", "rather", "regard", "say", "say", "see", "she", "so", "some", "take", "than", "that", "the", "their", "them", "then", "there", "these", "they", "think", "this", "thought", "time", "to", "took", "two", "up", "us", "use", "want", "way", "we", "well", "what", "when", "which", "who", "will", "with", "words", "work", "would", "year", "you", "your"] 
 
-# Read the filenames of the books so that we can ingest them
-bookDir = [fileName for fileName in os.listdir(path) if fileName.lower().endswith("txt")]
+# Read the sub directories
+subDirs = next(os.walk(path))[1]
 
-# The data structure that will hold the counts of words in each book that are inside or outside each concept
-probabilities = {fileName: { 
-    "community": { "words": communityWords, "count": [0, 0], "probability": 0 }, 
-    "religion": { "words": religionWords, "count": [0, 0], "probability": 0 } } for fileName in bookDir}
+# Read and process the books in each sub-directory
+for subDir in subDirs:
+    print(subDir + "\\Squished")
 
-# The data structure that will hold the counts of all the words in all the books
-wordCounts = {}
+    # Read the filenames of the books so that we can ingest them
+    bookDir = [fileName for fileName in os.listdir(path + subDir + "\\Squished") if fileName.lower().endswith("txt")]
 
-# Load each book and process it
-for book in bookDir:
-    print(book)
-    with io.open(path + book, "r", encoding="latin-1") as file:
-        fileTextRaw = file.read() # Open up the book and read in its entire contents
-    words = fileTextRaw.split() # Split into words
-    for word in words:
-        if not word in wordCounts:
-            wordCounts[word] = {}
-        if book in wordCounts[word]:
-            wordCounts[word][book] += 1 # Increment the word's count from this book in wordcounts
-        else:
-            wordCounts[word][book] = 1 # Add the word from this book to wordcounts
-        for categories, data in probabilities[book].items():
-            if word in data["words"]: # data["words"] is the concept list of words
-                data["count"] = [ data["count"][0] + 1, data["count"][1] ] # Increment the count of words in the concept
-            else:
-                data["count"] = [ data["count"][0], data["count"][1] + 1 ] # Increment the count of words outside the concept
-    for category, data in probabilities[book].items():
-        data["probability"] = float(data["count"][0]) / float(data["count"][0] + data["count"][1]) # Turn the counts into probabilities, i.e. the probability that a word, randomly chosen from this book, will be in this concept
-        print(category, data["probability"])
+    # The data structure that will hold the counts of words in each book that are inside or outside each concept
+    probabilities = {fileName: { 
+        "community": { "words": communityWords, "count": [0, 0], "probability": 0 }, 
+        "religion": { "words": religionWords, "count": [0, 0], "probability": 0 }, 
+        "social": { "words": socialWords, "count": [0, 0], "probability": 0 }, 
+        "family": { "words": familyWords, "count": [0, 0], "probability": 0 }, 
+        "economics": { "words": economicsWords, "count": [0, 0], "probability": 0 }, 
+        "education": { "words": educationWords, "count": [0, 0], "probability": 0 }, 
+        "health": { "words": healthWords, "count": [0, 0], "probability": 0 }, 
+        "crime": { "words": crimeWords, "count": [0, 0], "probability": 0 }, 
+        "environment": { "words": environmentWords, "count": [0, 0], "probability": 0 }, 
+        "welfare": { "words": welfareWords, "count": [0, 0], "probability": 0 }, 
+        "europe": { "words": europeWords, "count": [0, 0], "probability": 0 }, 
+        "foreignPolicy": { "words": foreignPolicyWords, "count": [0, 0], "probability": 0 }, 
+        "constitution": { "words": constitutionWords, "count": [0, 0], "probability": 0 }, 
+        "thirdWay": { "words": thirdWayWords, "count": [0, 0], "probability": 0 } } for fileName in bookDir}
 
-# Store the probabilities in a CSV file
-with io.open(path + "probabilities.csv", "w") as outputFile:
-    outputLines = [ u",community,religion" ]
+    # The data structure that will hold the counts of all the words in all the books
+    wordCounts = {}
+
+    # Load each book and process it
     for book in bookDir:
-        newOutputLine = book.replace(",", " ") + ","
-        newOutputLine = newOutputLine + str(probabilities[book]["community"]["probability"]) + ","
-        newOutputLine = newOutputLine + str(probabilities[book]["religion"]["probability"]) 
-        outputLines.append(newOutputLine)
-    mergedOutputLines = u"\n".join(outputLines)
-    outputFile.writelines(mergedOutputLines)
-
-# Store the word counts in a CSV file (in Excel we can add the 'Totals' column and sort rows by that total
-with io.open(path + "wordCounts.csv", "w") as outputFile:
-    outputLine = u"," + u",".join([ book.replace(",", " ") for book in bookDir ])
-    outputLines = [ outputLine ]
-    for word in wordCounts:
-        newLine = word
-        for book in bookDir:
+        print(book)
+        with io.open(path + subDir + "\\Squished\\" + book, "r", encoding="latin-1") as file:
+            fileTextRaw = file.read() # Open up the book and read in its entire contents
+        words = fileTextRaw.split() # Split into words
+        for word in words:
+            if not word in wordCounts:
+                wordCounts[word] = {}
             if book in wordCounts[word]:
-                newLine = newLine + u"," + str(wordCounts[word][book])
+                wordCounts[word][book] += 1 # Increment the word's count from this book in wordcounts
             else:
-                newLine = newLine + u",0" 
-        outputLines.append(newLine)
-    mergedOutputLines = u"\n".join(outputLines)
-    outputFile.writelines(mergedOutputLines)
+                wordCounts[word][book] = 1 # Add the word from this book to wordcounts
+            for categories, data in probabilities[book].items():
+                if word in data["words"]: # data["words"] is the concept list of words
+                    data["count"] = [ data["count"][0] + 1, data["count"][1] ] # Increment the count of words in the concept
+                else:
+                    data["count"] = [ data["count"][0], data["count"][1] + 1 ] # Increment the count of words outside the concept
+        for category, data in probabilities[book].items():
+            data["probability"] = 0
+            if float(data["count"][0]) != 0:
+                data["probability"] = float(data["count"][0]) / float(data["count"][0] + data["count"][1]) # Turn the counts into probabilities, i.e. the probability that a word, randomly chosen from this book, will be in this concept
+            print(category, data["probability"])
+
+    # Store the probabilities in a CSV file
+    with io.open(path + subDir + "\\probabilities.csv", "w") as outputFile:
+        outputLines = [ u",community,religion,social,family,economics,education,health,crime,environment,welfare,europe,foreign policy,constitution,third way" ]
+        for book in bookDir:
+            newOutputLine = book.replace(",", " ") + ","
+            newOutputLine = newOutputLine + str(probabilities[book]["community"]["probability"]) + ","
+            newOutputLine = newOutputLine + str(probabilities[book]["religion"]["probability"]) + ","
+            newOutputLine = newOutputLine + str(probabilities[book]["social"]["probability"]) + ","
+            newOutputLine = newOutputLine + str(probabilities[book]["family"]["probability"]) + ","
+            newOutputLine = newOutputLine + str(probabilities[book]["economics"]["probability"]) + ","
+            newOutputLine = newOutputLine + str(probabilities[book]["education"]["probability"]) + ","
+            newOutputLine = newOutputLine + str(probabilities[book]["health"]["probability"]) + ","
+            newOutputLine = newOutputLine + str(probabilities[book]["crime"]["probability"]) + ","
+            newOutputLine = newOutputLine + str(probabilities[book]["environment"]["probability"]) + ","
+            newOutputLine = newOutputLine + str(probabilities[book]["welfare"]["probability"]) + ","
+            newOutputLine = newOutputLine + str(probabilities[book]["europe"]["probability"]) + ","
+            newOutputLine = newOutputLine + str(probabilities[book]["foreignPolicy"]["probability"]) + ","
+            newOutputLine = newOutputLine + str(probabilities[book]["constitution"]["probability"]) + ","
+            newOutputLine = newOutputLine + str(probabilities[book]["thirdWay"]["probability"]) 
+            outputLines.append(newOutputLine)
+        mergedOutputLines = u"\n".join(outputLines)
+        outputFile.writelines(mergedOutputLines)
+
+    # Store the word counts in a CSV file (in Excel we can add the 'Totals' column and sort rows by that total
+    with io.open(path + subDir + "\\wordCounts.csv", "w") as outputFile:
+        outputLine = u"," + u",".join([ book.replace(",", " ") for book in bookDir ])
+        outputLines = [ outputLine ]
+        for word in wordCounts:
+            newLine = word
+            for book in bookDir:
+                if book in wordCounts[word]:
+                    newLine = newLine + u"," + str(wordCounts[word][book])
+                else:
+                    newLine = newLine + u",0" 
+            outputLines.append(newLine)
+        mergedOutputLines = u"\n".join(outputLines)
+        outputFile.writelines(mergedOutputLines)
 
 input("Done. Press 'return' to exit")
-
--- BookCounting/BookDistances.py
+++ BookCounting/BookDistances.py
@@ -2,43 +2,50 @@
 
 import os, io, csv
 
-path = "C:\\Users\\timregan\\Dropbox\\Projects\\Visualization\\Projectwork\\Miles2\\"
+path = "C:\\Users\\timregan\\Dropbox\\Projects\\Visualization\\Projectwork\\Miles2\\Texts\\"
 
-# Load in the Probabilities CSV file saved by the BookCounting.py file
-with io.open(path + "probabilities.csv", "r") as file:
-    lines = file.readlines()
-headers = lines[0].split(",")
-probabilities = {} # The data structure to store the probabilities read in from the CSV file
-bookNames = [] # List of the book titles for convenience as the keys in a dictionary data structure are not in a gaunteed order
-for line in lines[1:]:
-    splitLine = line.split(",")
-    probabilities[splitLine[0]] = {}
-    bookNames.append(splitLine[0])
-    for i in range(1, len(headers)):
-        key = headers[i]
-        value = float(splitLine[i])
-        probabilities[splitLine[0]][key] = value
+# Read the sub directories
+subDirs = next(os.walk(path))[1]
 
-distances = {} # The data structure to hold the results of the distance calculations
-for fromBook in probabilities:
-    distances[fromBook] = {}
-    for toBook in probabilities:
-        distance = float(0)
-        for concept in headers[1:]:
-            distance = distance + (probabilities[toBook][concept] - probabilities[fromBook][concept])**2 # The sum of the squares of the distances
-        distance = distance**0.5 # The square root of the sum of the squares of the distances
-        distances[fromBook][toBook] = distance
+# Read and process the books in each sub-directory
+for subDir in subDirs:
+    print(subDir)
 
-# Store the distances in a CSV file
-with io.open(path + "distances.csv", "w") as outputFile:
-    outputLine = u"," + u",".join(probabilities)
-    outputLines = [ outputLine ]
-    for fromBook in bookNames:
-        newLine = fromBook
-        for toBook in bookNames:
-            newLine = newLine + u"," + str(distances[fromBook][toBook])
-        outputLines.append(newLine)
-    mergedOutputLines = u"\n".join(outputLines)
-    outputFile.writelines(mergedOutputLines)
+    # Load in the Probabilities CSV file saved by the BookCounting.py file
+    with io.open(path + "probabilities.csv", "r") as file:
+        lines = file.readlines()
+    headers = lines[0].split(",")
+    probabilities = {} # The data structure to store the probabilities read in from the CSV file
+    bookNames = [] # List of the book titles for convenience as the keys in a dictionary data structure are not in a gaunteed order
+    for line in lines[1:]:
+        splitLine = line.split(",")
+        probabilities[splitLine[0]] = {}
+        bookNames.append(splitLine[0])
+        for i in range(1, len(headers)):
+            key = headers[i]
+            value = float(splitLine[i])
+            probabilities[splitLine[0]][key] = value
+
+    distances = {} # The data structure to hold the results of the distance calculations
+    for fromBook in probabilities:
+        distances[fromBook] = {}
+        for toBook in probabilities:
+            distance = float(0)
+            for concept in headers[1:]:
+                distance = distance + (probabilities[toBook][concept] - probabilities[fromBook][concept])**2 # The sum of the squares of the distances
+            distance = distance**0.5 # The square root of the sum of the squares of the distances
+            distances[fromBook][toBook] = distance
+
+    # Store the distances in a CSV file
+    with io.open(path + "distances.csv", "w") as outputFile:
+        outputLine = u"," + u",".join(probabilities)
+        outputLines = [ outputLine ]
+        for fromBook in bookNames:
+            newLine = fromBook
+            for toBook in bookNames:
+                newLine = newLine + u"," + str(distances[fromBook][toBook])
+            outputLines.append(newLine)
+        mergedOutputLines = u"\n".join(outputLines)
+        outputFile.writelines(mergedOutputLines)
 
 input("Done. Press 'return' to exit")
