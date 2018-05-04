ff98b26731c14a3d4eda9b5c0e1c371136aef5b2
Tim Regan <timregan@microsoft.com>
2018-05-04 16:33:35 +0100

    Squishing for Kant, i.e. no stop words

--- BookCounting/BookSquishing.py
+++ BookCounting/BookSquishing.py
@@ -1,13 +1,6 @@
 import os, io, re
 from functools import reduce
 
-path = "C:\\Users\\timregan\\Dropbox\\Projects\\Visualization\\Projectwork\\Miles2\\Texts\\"
-
-# Generic stop words and specific words to ignore. N.B. we cannot include "labour" since we need to keep "new labour"
-stopWords = ["a", "about", "after", "all", "also", "an", "and", "any", "around ", "as", "at", "back", "be", "be", "because", "blair", "but", "by", "can", "come", "conservative", "could", "course", "day", "do", "do", "even", "ever", "first", "first", "for", "from", "get", "get", "give", "go", "good", "got", "great", "have", "he", "her", "him", "his", "how", "however", "i", "if", "important", "in", "into", "it", "its", "just", "just", "know", "labour", "like", "look", "major", "make", "make", "may", "me", "means", "measures", "might", "most", "my", "new", "no", "not", "now", "of", "on", "one", "only", "or", "other", "our", "out", "over", "people", "plan", "rather", "regard", "say", "say", "see", "she", "so", "some", "take", "than", "that", "the", "their", "them", "then", "there", "these", "they", "think", "this", "thought", "time", "to", "took", "two", "up", "us", "use", "want", "way", "we", "well", "what", "when", "which", "who", "will", "with", "words", "work", "would", "year", "you", "your"] 
-stopPhrases = ["conservative party", "john major", "labour party", "tony blair"]
-conflate = [["new labour", "newlabour"]]
-
 # The function we use to tidy the books into a manageable string
 def squish(string, char, keepHyphens = False, keepSentenceEndings = False):
     if char == u"’":
@@ -23,42 +16,50 @@ def squish(string, char, keepHyphens = False, keepSentenceEndings = False):
     else:
         return string # ignore everything else
 
-# Read the sub directories
-subDirs = next(os.walk(path))[1]
+# Squish each txt file in any sub directories and optionally remove the stop words etc.
+def squishInDirs(path, loseStopWords = True):
+    # Generic stop words and specific words to ignore. N.B. we cannot include "labour" since we need to keep "new labour"
+    stopWords = ["a", "about", "after", "all", "also", "an", "and", "any", "around ", "as", "at", "back", "be", "be", "because", "blair", "but", "by", "can", "come", "conservative", "could", "course", "day", "do", "do", "even", "ever", "first", "first", "for", "from", "get", "get", "give", "go", "good", "got", "great", "have", "he", "her", "him", "his", "how", "however", "i", "if", "important", "in", "into", "it", "its", "just", "just", "know", "labour", "like", "look", "major", "make", "make", "may", "me", "means", "measures", "might", "most", "my", "new", "no", "not", "now", "of", "on", "one", "only", "or", "other", "our", "out", "over", "people", "plan", "rather", "regard", "say", "say", "see", "she", "so", "some", "take", "than", "that", "the", "their", "them", "then", "there", "these", "they", "think", "this", "thought", "time", "to", "took", "two", "up", "us", "use", "want", "way", "we", "well", "what", "when", "which", "who", "will", "with", "words", "work", "would", "year", "you", "your"] 
+    stopPhrases = ["conservative party", "john major", "labour party", "tony blair"]
+    conflate = [["new labour", "newlabour"]]
 
-# Read and process the books in each sub-directory
-for subDir in subDirs:
-	print(subDir)
+    # Read the sub directories
+    subDirs = next(os.walk(path))[1]
 
-	# Read the filenames of the books so that we can ingest them
-	bookDir = [fileName for fileName in os.listdir(path + subDir) if fileName.lower().endswith("txt")]
+    # Read and process the books in each sub-directory
+    for subDir in subDirs:
+        print(subDir)
 
-	# If the squish directory does not exist then create it
-	if not os.path.exists(path + subDir + "\\Squished"):
-		os.makedirs(path + subDir +"\\Squished")
+        # Read the filenames of the books so that we can ingest them
+        bookDir = [fileName for fileName in os.listdir(path + subDir) if fileName.lower().endswith("txt")]
 
-	# Load each book and squish it
-	for book in bookDir:
-		print(book)
-		#Only squish the book if it is not done already
-		if not os.path.exists(path + subDir + "\\Squished\\" + book): 
-			with open(path + subDir + "\\" + book, "r", encoding="latin-1") as file:
-				fileTextRaw = file.read() # Open up the book and read in its entire contents
-			fileText = str.lower(reduce(squish, fileTextRaw)) # Clean it up using the squish function above
-			for conflation in conflate:
-				fileText = fileText.replace(conflation[0], conflation[1])
-			for stopPhrase in stopPhrases:
-				fileText = fileText.replace(stopPhrase, "")
-			allWords = fileText.split() # Split into words (or at least runs of characters delimeted by spaces)
-			print("length (all words) = ", len(allWords))
-			words = [word for word in allWords if not word in stopWords] # Remove the 'stop' words
-			print("length (without stop words) = ", len(words))
-			words = [word for word in words if len(word.encode("utf-8")) >= 3] # Remove short words that are only one or two characters long #BUG: Why is the 'word' aï making it through?
-			print("length (without tiny words) = ", len(words))
-			# Store the squished book
-			with io.open(path + subDir + "\\Squished\\" + book, "w") as outputFile:
-				line = u" ".join(words)
-				outputFile.writelines(line)
+        # If the squish directory does not exist then create it
+        if not os.path.exists(path + subDir + "\\Squished"):
+            os.makedirs(path + subDir +"\\Squished")
 
-input("Done. Press 'return' to exit")
+        # Load each book and squish it
+        for book in bookDir:
+            print(book)
+            # Only squish the book if it is not done already
+            if not os.path.exists(path + subDir + "\\Squished\\" + book): 
+                with open(path + subDir + "\\" + book, "r", encoding="latin-1") as file:
+                    fileTextRaw = file.read() # Open up the book and read in its entire contents
+                fileText = str.lower(reduce(squish, fileTextRaw)) # Clean it up using the squish function above
+                if loseStopWords:
+                    for conflation in conflate:
+                        fileText = fileText.replace(conflation[0], conflation[1])
+                    for stopPhrase in stopPhrases:
+                        fileText = fileText.replace(stopPhrase, "")
+                allWords = fileText.split() # Split into words (or at least runs of characters delimeted by spaces)
+                print("length (all words) = ", len(allWords))
+                words = allWords
+                if loseStopWords:
+                    words = [word for word in allWords if not word in stopWords] # Remove the 'stop' words
+                    print("length (without stop words) = ", len(words))
+                words = [word for word in words if len(word.encode("utf-8")) >= 3] # Remove short words that are only one or two characters long #BUG: Why is the 'word' aï making it through?
+                print("length (without tiny words) = ", len(words))
+                # Store the squished book
+                with io.open(path + subDir + "\\Squished\\" + book, "w") as outputFile:
+                    line = u" ".join(words)
+                    outputFile.writelines(line)
 
---
+++ BookCounting/tfidfDistance.py
@@ -0,0 +1,5 @@
+
+
+from BookSquishing import squish, squishInDirs
+
+squishInDirs('C:\\Users\\timregan\\Dropbox\\Projects\\Visualization\\Projectwork\\Miles3\\', False)
